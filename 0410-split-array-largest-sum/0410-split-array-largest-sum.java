8f6bb6d45cff3a39df5b1866d710c78844fcbd21
jeetjajal <jajaljeet2005@gmail.com>
2026-08-22 12:05:53 +0530

    Time: 1 ms (56.34%), Space: 42.8 MB (84.43%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,39 +1,37 @@
 class Solution {
-
-    public boolean isPossible(int[] arr, int k, int workload) {
-        int count = 1;
-        int currentWork = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (currentWork + arr[i] <= workload) {
-                currentWork += arr[i];
-            } else {
+    public static boolean ispossible(int[] arr, int k,int workload) {
+        int n=arr.length;
+        int count=1,currentwork=0;
+        for(int i=0;i<n;i++){
+            if(currentwork+arr[i]<=workload){
+                currentwork+=arr[i];
+            }
+            else{
                 count++;
-                currentWork = arr[i];
+                currentwork=arr[i];
             }
         }
-
-        return count <= k;
+        return count<=k;
     }
-
-    public int splitArray(int[] nums, int k) {
-        int n = nums.length;
-        int sum = 0;
-        int maxVal = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            maxVal = Math.max(maxVal, nums[i]);
+    public int splitArray(int arr[],int k){
+        int n=arr.length;
+        if(k>n){
+            return -1;
+        }       
+        int sum=0, maxval=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            maxval=Math.max(maxval,arr[i]);
         }
-        int st = maxVal, end = sum;
-        int ans = -1;
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-            if (isPossible(nums, k, mid)) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                st = mid + 1;
+        int st=maxval,end=sum,ans=-1;
+        while(st<=end){
+            int   mid=st+(end-st)/2;
+            if(ispossible(arr,k,mid)){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
             }
         }
         return ans;
