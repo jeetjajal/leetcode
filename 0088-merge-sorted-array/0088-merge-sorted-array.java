8cdfbeb664415eee483e05a3cf15d7201fd9388c
jeetjajal <jajaljeet2005@gmail.com>
2026-08-25 18:37:19 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void merge(int[] a, int m, int[] b, int n) {
-        int idx=m+n-1,i=m-1,j=n-1;
+        int idx=m+n-1,i=m-1,j=n-1;//start from backword
 
         while(i>=0 && j>=0){
             if(a[i]>b[j]){
